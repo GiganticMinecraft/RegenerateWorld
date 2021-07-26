5cf71248d2c951ed4a405065e9dfc39bd9ff3846
Lucky <lucky3028@users.noreply.github.com>
2021-07-26 20:13:16 +0900

    [Add] IError.kt
    enum classにextendすることで、sender.sendMessageとlogger.severeをワンライナーでできる

---
+++ src/main/kotlin/click/seichi/regenerateworld/IError.kt
@@ -0,0 +1,19 @@
+package click.seichi.regenerateworld
+
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
+import org.bukkit.command.CommandSender
+
+interface IError {
+    fun errorName(): String
+    fun reason(): String
+    fun withLog(sender: CommandSender): IError = run {
+        sender.sendMessage("${ChatColor.RED}${reason()}")
+        return this
+    }
+    fun withServerLog(): IError = run {
+        setOf("処理中にエラーが発生しました。", "${errorName()}: ${reason()}")
+            .forEach { Bukkit.getServer().logger.severe(it) }
+        return this
+    }
+}
