2933dbe227733a9f9f5be5f9d2ded9bf1dd7d46b
Lucky <lucky3028@users.noreply.github.com>
2021-07-26 20:30:31 +0900

    [Fix] format

--- src/main/kotlin/click/seichi/regenerateworld/Multiverse.kt
+++ src/main/kotlin/click/seichi/regenerateworld/Multiverse.kt
@@ -2,9 +2,7 @@ package click.seichi.regenerateworld
 
 import click.seichi.regenerateworld.events.PreRegenerateWorldEvent
 import click.seichi.regenerateworld.events.RegenerateWorldEvent
-import com.github.michaelbull.result.Err
-import com.github.michaelbull.result.Ok
-import com.github.michaelbull.result.Result
+import com.github.michaelbull.result.*
 import com.onarandombox.MultiverseCore.MultiverseCore
 import com.onarandombox.MultiverseCore.api.MultiverseWorld
 import org.bukkit.Bukkit
@@ -22,7 +20,12 @@ object Multiverse {
 
     fun findMvWorld(world: World): MultiverseWorld? = instance.mvWorldManager.getMVWorld(world)
 
-    private fun regenWorld(world: MultiverseWorld, isNewSeed: Boolean, isRandomSeed: Boolean, seed: String?) {
+    private fun regenWorld(
+        world: MultiverseWorld,
+        isNewSeed: Boolean,
+        isRandomSeed: Boolean,
+        seed: String?
+    ) {
         val preEvent = PreRegenerateWorldEvent(world.name)
         Bukkit.getPluginManager().callEvent(preEvent)
         if (preEvent.isCancelled) return
@@ -32,13 +35,19 @@ object Multiverse {
         Bukkit.getPluginManager().callEvent(RegenerateWorldEvent(world.name))
     }
 
-    fun regenWorld(world: MultiverseWorld, seedType: SeedType, seed: String? = null): Result<Unit, IError> =
+    fun regenWorld(
+        world: MultiverseWorld,
+        seedType: SeedType,
+        seed: String? = null
+    ): Result<Unit, MultiverseError> =
         if (seedType == SeedType.NEW_SEED && seed == null) Err(MultiverseError.SEED_IS_NON_NULL)
         else Ok(regenWorld(world, seedType.isNewSeed, seedType.isRandomSeed, seed))
 
     fun getSpawnWorld(): MultiverseWorld = instance.mvWorldManager.spawnWorld
 
-    fun setSpawnLocation(world: MultiverseWorld, location: Location) { world.spawnLocation = location }
+    fun setSpawnLocation(world: MultiverseWorld, location: Location) {
+        world.spawnLocation = location
+    }
 }
 
 enum class SeedType(val isNewSeed: Boolean, val isRandomSeed: Boolean) {
