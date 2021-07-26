4a3dbc027d2a32fbf20442fd56a7ee1fedcbc4bf
Lucky <lucky3028@users.noreply.github.com>
2021-07-26 12:21:55 +0900

    [Add] MultiverseCoreの再生成処理をwrap

--- src/main/kotlin/click/seichi/regenerateworld/Multiverse.kt
+++ src/main/kotlin/click/seichi/regenerateworld/Multiverse.kt
@@ -1,6 +1,9 @@
 package click.seichi.regenerateworld
 
 import com.onarandombox.MultiverseCore.MultiverseCore
+import com.onarandombox.MultiverseCore.api.MultiverseWorld
+import org.bukkit.Location
+import org.bukkit.World
 
 object Multiverse {
     private lateinit var instance: MultiverseCore
@@ -10,4 +13,20 @@ object Multiverse {
 
         if (plugin != null && plugin is MultiverseCore) instance = plugin
     }
+
+    fun findMvWorld(world: World): MultiverseWorld? = instance.mvWorldManager.getMVWorld(world)
+
+    private fun regenWorld(world: MultiverseWorld, isNewSeed: Boolean, isRandomSeed: Boolean, seed: String?) =
+        instance.mvWorldManager.regenWorld(world.name, isNewSeed, isRandomSeed, seed)
+
+    fun regenWorldWithCurrentSeed(world: MultiverseWorld) =
+        regenWorld(world, isNewSeed = false, isRandomSeed = false, seed = null)
+
+    fun regenWorldWithNewSeed(world: MultiverseWorld, seed: String) =
+        regenWorld(world, isNewSeed = true, isRandomSeed = false, seed)
+
+    fun regenWorldWithRandomNewSeed(world: MultiverseWorld) =
+        regenWorld(world, isNewSeed = true, isRandomSeed = true, seed = null)
+
+    fun setSpawnLocation(world: MultiverseWorld, location: Location) { world.spawnLocation = location }
 }
