9ff908e559fc3f8e6b4ab1c1d9dcafd8a0d76c46
Lucky <lucky3028@users.noreply.github.com>
2021-07-28 22:37:15 +0900

    [Add] 再生成をリスケする関数

---
+++ src/main/kotlin/click/seichi/regenerateworld/Util.kt
@@ -0,0 +1,26 @@
+package click.seichi.regenerateworld
+
+import org.bukkit.Bukkit
+import java.time.Duration
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
+object Util {
+    fun scheduleRegenerateTask(plan: Plan) {
+        Bukkit.getScheduler().cancelTask(plan.taskId)
+        val now = ZonedDateTime.now()
+        val nextDate = now.plusMinutes(plan.interval)
+        val taskId = RegenerateTask(plan).runTaskTimer(
+            RegenerateWorld.plugin,
+            Duration.between(now, nextDate).toMinutes() * 60 * 20,
+            20L * 1
+        ).taskId
+        Bukkit.getServer().logger.info(
+            "次回再生成は「${nextDate.format(DateTimeFormatter.ISO_LOCAL_DATE)} ${
+                nextDate.format(DateTimeFormatter.ISO_LOCAL_TIME)
+            }」"
+        )
+        Config.setData(PathType.TASK_ID, plan.id, taskId)
+        Config.setData(PathType.DATE, plan.id, nextDate.toString())
+    }
+}
