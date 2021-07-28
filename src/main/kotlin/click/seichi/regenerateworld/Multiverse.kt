95308a767286cb610660a8bd7036109b85d96438
Lucky <lucky3028@users.noreply.github.com>
2021-07-28 23:16:18 +0900

    [Add] SEED値の指定が必要かどうかを判定するメソッド

--- src/main/kotlin/click/seichi/regenerateworld/Multiverse.kt
+++ src/main/kotlin/click/seichi/regenerateworld/Multiverse.kt
@@ -62,7 +62,9 @@ object Multiverse {
 enum class SeedType(val isNewSeed: Boolean, val isRandomSeed: Boolean) {
     CURRENT_SEED(false, false),
     NEW_SEED(true, false),
-    RANDOM_NEW_SEED(true, true)
+    RANDOM_NEW_SEED(true, true);
+
+    fun isSeedNecessary() = this == NEW_SEED
 }
 
 enum class MultiverseError(private val reason: String) : IError {
