e4102dd94df1f9b6b3cc3cf8480aa59e0a394b55
Lucky <lucky3028@users.noreply.github.com>
2021-07-28 22:38:47 +0900

    [Add] 再生成を起爆

--- src/main/kotlin/click/seichi/regenerateworld/RegenerateWorld.kt
+++ src/main/kotlin/click/seichi/regenerateworld/RegenerateWorld.kt
@@ -2,7 +2,9 @@ package click.seichi.regenerateworld
 
 import click.seichi.regenerateworld.commands.RegenerateCommand
 import click.seichi.regenerateworld.listener.RegenerateWorldEventListener
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
+import java.time.ZonedDateTime
 
 class RegenerateWorld : JavaPlugin() {
     companion object {
@@ -13,12 +15,23 @@ class RegenerateWorld : JavaPlugin() {
     override fun onEnable() {
         plugin = this
         Multiverse.load()
+        Config.load()
+        // TODO: debug
+        println(Config.loadPlans())
+
+        Config.loadPlans().forEach {
+            val now = ZonedDateTime.now()
+            if (it.date.isBefore(now)) {
+                Config.setData(PathType.DATE, it.id, now.toString())
+                RegenerateTask(it).runTaskTimer(plugin, 0, 20L * 1) //TODO: 1secごとなので1minに
+            } else Util.scheduleRegenerateTask(it)
+        }
 
         getCommand("regenerateworld").executor = RegenerateCommand
         server.pluginManager.registerEvents(RegenerateWorldEventListener, this)
     }
 
     override fun onDisable() {
-        // Plugin shutdown logic
+        Config.loadPlans().map { it.taskId }.forEach { Bukkit.getScheduler().cancelTask(it) }
     }
 }
