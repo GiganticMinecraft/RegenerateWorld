da0306ebf316a9491b7898a208262686ff55ebb1
Lucky <lucky3028@users.noreply.github.com>
2021-07-28 23:58:19 +0900

    [Add] docs

--- src/main/kotlin/click/seichi/regenerateworld/utils/ConfigPaths.kt
+++ src/main/kotlin/click/seichi/regenerateworld/utils/ConfigPaths.kt
@@ -8,6 +8,15 @@ import click.seichi.regenerateworld.PLANS_SECTION_NAME
 enum class ConfigPaths {
     BUKKIT_TASK_ID, DATE, INTERVAL, SEED_TYPE, SEED, WORLDS;
 
+    /**
+     * サブ階層のみ。
+     * @param id UniqueID。
+     */
     fun shortPath(id: String) = "$id.${this.name.lowercase()}"
+
+    /**
+     * ルートからの絶対パスのような指定。すべての階層を含む。
+     * @param id UniqueID。
+     */
     fun fullPath(id: String) = "$PLANS_SECTION_NAME.${shortPath(id)}"
 }
