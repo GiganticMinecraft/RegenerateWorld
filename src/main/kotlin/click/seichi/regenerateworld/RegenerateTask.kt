27d6a9518caec4275d3445504b6ce84b3c2e00a2
Lucky <lucky3028@users.noreply.github.com>
2021-07-28 23:21:22 +0900

    [Add] ワールドを再生成するBukkitRunnable

---
+++ src/main/kotlin/click/seichi/regenerateworld/RegenerateTask.kt
@@ -0,0 +1,49 @@
+package click.seichi.regenerateworld
+
+import click.seichi.regenerateworld.utils.Logger
+import click.seichi.regenerateworld.utils.Util
+import com.github.michaelbull.result.get
+import com.github.michaelbull.result.mapBoth
+import com.github.michaelbull.result.mapError
+import org.bukkit.Bukkit
+import org.bukkit.scheduler.BukkitRunnable
+
+class RegenerateTask(val plan: Plan) : BukkitRunnable() {
+    private val warningMinutes = setOf(1, 3, 5, 10)
+    private var count: Int = 10
+
+    override fun run() {
+        // FIXME remove debug
+        println("task was running")
+        println(count)
+        val worldNames = plan.worlds.mapNotNull { Multiverse.findMvWorld(it).get() }.map { it.name }
+        if (warningMinutes.contains(count)) {
+            Bukkit.broadcastMessage("あと${count}分で${worldNames}の再生成が行われます。")
+        } else if (count == 0) {
+            object : BukkitRunnable() {
+                override fun run() {
+                    // 実際に再生成を行うまで時間があるので、この間にMultiverseからアンロードされている可能性を考えて、生成を行うときには再度MultiverseWorldを探す
+                    plan.worlds.mapNotNull {
+                        Multiverse.findMvWorld(it)
+                            .mapError { err -> err.withServerLog("ワールド名: $it") }.get()
+                    }.forEach { world ->
+                        Multiverse.regenWorld(world, plan.seedType, plan.seed).mapBoth(
+                            success = {
+                                val msg = "${world.name}の再生成を行いました。"
+                                Logger.info(msg)
+                                Bukkit.broadcastMessage(msg)
+                            },
+                            failure = { err -> err.withServerLog("ID: ${plan.id}") }
+                        )
+                    }
+
+                    val nextDate = plan.date.plusMinutes(plan.interval)
+                    Config.setData(PathType.DATE, plan.id, nextDate.toString())
+                    Util.scheduleRegenerateTask(plan)
+                }
+            }.runTask(RegenerateWorld.plugin)
+            this.cancel()
+        }
+        count -= 1
+    }
+}
