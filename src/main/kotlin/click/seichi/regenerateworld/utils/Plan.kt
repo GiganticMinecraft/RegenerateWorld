ad9f0c466ae0c8d8831c473ef2793ace8c7e7532
Lucky <lucky3028@users.noreply.github.com>
2021-07-28 23:49:26 +0900

    [Add] docs

--- src/main/kotlin/click/seichi/regenerateworld/utils/Plan.kt
+++ src/main/kotlin/click/seichi/regenerateworld/utils/Plan.kt
@@ -10,26 +10,32 @@ data class Plan(
      * UniqueID。TODO: 自動で割り振るUUID
      */
     val id: String,
+
     /**
      * BukkitScheduler上のタスクID。
      */
     val bukkitTaskId: Int,
+
     /**
      * 最後に再生成を実行した日時。
      */
     val lastRegeneratedDate: ZonedDateTime,
+
     /**
      * 再生成を行う間隔。
      */
     val interval: Long,
+
     /**
      * Seed値に関する設定。詳細は[[SeedType]]を参照。
      */
     val seedType: SeedType,
+
     /**
      * Seed値を設定する。詳細は[[SeedType]]を参照。
      */
     val seed: String?,
+
     /**
      * 再生成を行うワールド群。
      */
