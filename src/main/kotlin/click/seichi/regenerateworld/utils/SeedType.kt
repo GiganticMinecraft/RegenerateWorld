58a846aba0a07c91a7ea3fbe44b3c2249f0a00bd
Lucky <lucky3028@users.noreply.github.com>
2021-07-28 23:48:05 +0900

    [Add] docs

--- src/main/kotlin/click/seichi/regenerateworld/utils/SeedType.kt
+++ src/main/kotlin/click/seichi/regenerateworld/utils/SeedType.kt
@@ -1,9 +1,28 @@
 package click.seichi.regenerateworld.utils
 
+/**
+ * Seed値のパターンをまとめた列挙クラス。
+ * @param isNewSeed 新しいSeed値を使うかどうか。
+ * @param isRandomSeed Seed値をランダムに決めるかどうか。
+ */
 enum class SeedType(val isNewSeed: Boolean, val isRandomSeed: Boolean) {
+    /**
+     * 現在のSeed値をそのまま利用する。
+     */
     CURRENT_SEED(false, false),
+
+    /**
+     * Seed値を新しく生成し、その値は自分で指定したものを用いる。
+     */
     NEW_SEED(true, false),
+
+    /**
+     * Seed値を新しく生成し、その値はランダムで生成する。
+     */
     RANDOM_NEW_SEED(true, true);
 
+    /**
+     * Seed値の設定が必要かどうか。
+     */
     fun isSeedNecessary() = this == NEW_SEED
 }
