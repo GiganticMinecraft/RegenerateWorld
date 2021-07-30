10b6febbf2796d64863579863d9d87f4896b5225
Lucky <lucky3028@users.noreply.github.com>
2021-07-30 22:20:55 +0900

    [Change] ConfigからPlanに加工する際に型チェックを行うようにする

--- src/main/kotlin/click/seichi/regenerateworld/Config.kt
+++ src/main/kotlin/click/seichi/regenerateworld/Config.kt
@@ -1,8 +1,7 @@
 package click.seichi.regenerateworld
 
-import click.seichi.regenerateworld.utils.ConfigPaths
-import click.seichi.regenerateworld.utils.Plan
-import click.seichi.regenerateworld.utils.SeedPatterns
+import click.seichi.regenerateworld.utils.*
+import org.bukkit.configuration.ConfigurationSection
 import org.bukkit.configuration.file.FileConfiguration
 import java.time.ZonedDateTime
 
@@ -16,25 +15,51 @@ object Config {
             it.config
         }
     }
+    private val plansSection: ConfigurationSection? by lazy {
+        config.getConfigurationSection(
+            PLANS_SECTION_NAME
+        )
+    }
 
-    fun loadPlans(): List<Plan> {
-        val plans = config.getConfigurationSection(PLANS_SECTION_NAME)
-        return plans.getKeys(false).map { id ->
-            // TODO 各項目の存在確認
+    /**
+     * 各項目に対して、ConfigPaths.existsを実行して、すべてtrueではない（存在しないか型が合わない）PlanのListを返す
+     */
+    private fun filterBadPlanConfigs(plans: ConfigurationSection) = plans.getKeys(false).map { id ->
+        id to ConfigPaths.values()
+            .map { path -> path to plans.get(path.shortPath(id)) }
+            .map { (path, value) -> path to path.exists(value) }
+            .filterNot { it.second }
+    }.filterNot { it.second.isEmpty() }.map { it.first to it.second.map { (paths, _) -> paths } }
+
+    fun loadPlans() = plansSection?.let { plans ->
+        val invalidPlanConfigs = filterBadPlanConfigs(plans)
+        val validPlanIds =
+            plans.getKeys(false).filterNot { invalidPlanConfigs.map { (id, _) -> id }.contains(it) }
+
+        if (invalidPlanConfigs.isEmpty() || validPlanIds.isEmpty())
+            Logger.infoWithPrefix("すべての再生成スケジュールを読み込みました。")
+        else invalidPlanConfigs.forEach { (id, pathsList) ->
+            ConfigError.PATH_IS_NOT_FOUND.withServerLog("id: $id, paths: $pathsList")
+        }
+
+        validPlanIds.map { id ->
             val taskId = plans.getInt(ConfigPaths.TASK_ID.shortPath(id))
-            val date = ZonedDateTime.parse(
-                plans.getString(ConfigPaths.DATE.shortPath(id)).replace("=", "T")
-            )
+            val date = ZonedDateTime.parse(plans.getString(ConfigPaths.DATE.shortPath(id)))
             val interval = plans.getLong(ConfigPaths.INTERVAL.shortPath(id))
             val seedPattern =
                 SeedPatterns.valueOf(plans.getString(ConfigPaths.SEED_TYPE.shortPath(id)))
             val seed =
                 if (seedPattern.isSeedNecessary()) plans.getString(ConfigPaths.SEED.shortPath(id)) else null
+            // Worldsは、実際に再生成を行う時にならないとMultiverseのロード状況が不明のため、Stringのまま
             val worlds =
-                plans.getList(ConfigPaths.WORLDS.shortPath(id)).toList().filterIsInstance<String>()
+                plans.getList(ConfigPaths.WORLDS.shortPath(id)).toList()
+                    .filterIsInstance<String>()
 
             Plan(id, taskId, date, interval, seedPattern, seed, worlds)
         }
+    } ?: run {
+        ConfigError.PATH_IS_NOT_FOUND.withServerLog("id: $PLANS_SECTION_NAME")
+        listOf()
     }
 
     // TODO valueの型を限定的にする
@@ -43,4 +68,11 @@ object Config {
         config.set(configPath.fullPath(id), value)
         RegenerateWorld.plugin.saveConfig()
     }
+}
+
+enum class ConfigError(val reason: String) : IError {
+    PATH_IS_NOT_FOUND("指定されたパスはConfigに存在しません。");
+
+    override fun errorName() = this.name
+    override fun reason() = this.reason
 }
