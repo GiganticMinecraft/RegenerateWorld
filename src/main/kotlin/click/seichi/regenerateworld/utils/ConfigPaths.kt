e4a3ac8f46c319242f0288a0d0e5865dcefdfc94
Lucky <lucky3028@users.noreply.github.com>
2021-07-30 22:18:25 +0900

    [Add] COnfigPathsにそれぞれの型チェックを行える関数を追加

--- src/main/kotlin/click/seichi/regenerateworld/utils/ConfigPaths.kt
+++ src/main/kotlin/click/seichi/regenerateworld/utils/ConfigPaths.kt
@@ -6,7 +6,25 @@ import click.seichi.regenerateworld.PLANS_SECTION_NAME
  * Configの階層をたどるのに度々書いていると間違えるので、列挙クラス化。詳細の項目については、[[Plan]]を参照。
  */
 enum class ConfigPaths {
-    TASK_ID, DATE, INTERVAL, SEED_TYPE, SEED, WORLDS;
+    TASK_ID {
+        override fun exists(value: Any?) = value is Int
+    },
+    DATE {
+        override fun exists(value: Any?) = value is String
+    },
+    INTERVAL {
+        override fun exists(value: Any?) = value is Int
+    },
+    SEED_TYPE {
+        override fun exists(value: Any?) =
+            value is String && SeedPatterns.safeValueOf(value) != null
+    },
+    SEED {
+        override fun exists(value: Any?) = value is String?
+    },
+    WORLDS {
+        override fun exists(value: Any?) = value is List<*> && value.all { it is String }
+    };
 
     /**
      * サブ階層のみへのパス文字列を返す。
@@ -19,4 +37,6 @@ enum class ConfigPaths {
      * @param id UniqueID。
      */
     fun fullPath(id: String) = "$PLANS_SECTION_NAME.${shortPath(id)}"
+
+    abstract fun exists(value: Any?): Boolean
 }
