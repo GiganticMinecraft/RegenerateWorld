3969408edf5439fbcc4f97d705c1ca7a6d11e69a
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-08-22 15:38:53 +0900

    feat: add presenter

---
+++ src/main/scala/click/seichi/regenerateworld/presenter/Command.scala
@@ -0,0 +1,17 @@
+package click.seichi.regenerateworld.presenter
+
+import click.seichi.regenerateworld.presenter.injector.GenerationScheduleUseCase
+import org.bukkit.command.{Command, CommandSender, TabExecutor}
+
+import java.util
+import java.util.Collections.emptyList
+
+object Command extends TabExecutor {
+  override def onCommand(commandSender: CommandSender, command: Command, s: String, strings: Array[String]): Boolean = {
+    val list = GenerationScheduleUseCase.list()
+    println(list)
+    true
+  }
+
+  override def onTabComplete(commandSender: CommandSender, command: Command, s: String, strings: Array[String]): util.List[String] = emptyList()
+}
