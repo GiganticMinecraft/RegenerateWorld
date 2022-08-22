93db219e3d9e50eea166bafd1a4f90b73401355a
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-08-22 22:24:18 +0900

    fix: save default config on enable

--- src/main/scala/click/seichi/regenerateworld/RegenerateWorld.scala
+++ src/main/scala/click/seichi/regenerateworld/RegenerateWorld.scala
@@ -1,15 +1,13 @@
 package click.seichi.regenerateworld
 
-import click.seichi.regenerateworld.infra.model.GenerationScheduleConfig
 import click.seichi.regenerateworld.presenter.Command
-import org.bukkit.configuration.serialization.ConfigurationSerialization
 import org.bukkit.plugin.java.JavaPlugin
 
 class RegenerateWorld extends JavaPlugin {
   override def onEnable(): Unit = {
     RegenerateWorld.INSTANCE = this
 
-    ConfigurationSerialization.registerClass(classOf[GenerationScheduleConfig])
+    saveDefaultConfig()
 
     getCommand("regenworld").setExecutor(Command)
   }
