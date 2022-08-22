979d68f7a0fff21eee2a6d9c8f2003beb4701cf4
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-08-22 15:39:03 +0900

    feat: add main class

---
+++ src/main/scala/click/seichi/regenerateworld/RegenerateWorld.scala
@@ -0,0 +1,20 @@
+package click.seichi.regenerateworld
+
+import click.seichi.regenerateworld.infra.model.GenerationScheduleConfig
+import click.seichi.regenerateworld.presenter.Command
+import org.bukkit.configuration.serialization.ConfigurationSerialization
+import org.bukkit.plugin.java.JavaPlugin
+
+class RegenerateWorld extends JavaPlugin {
+  override def onEnable(): Unit = {
+    RegenerateWorld.INSTANCE = this
+
+    ConfigurationSerialization.registerClass(classOf[GenerationScheduleConfig])
+
+    getCommand("/regenworld").setExecutor(Command)
+  }
+}
+
+object RegenerateWorld {
+  var INSTANCE: RegenerateWorld = _
+}
