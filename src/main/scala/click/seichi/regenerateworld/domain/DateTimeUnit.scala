9cbaa654b18ab2d1a211be8fc49689a619b41530
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-08-23 16:21:27 +0900

    feat: add DateTimeUnit enum

---
+++ src/main/scala/click/seichi/regenerateworld/domain/DateTimeUnit.scala
@@ -0,0 +1,25 @@
+package click.seichi.regenerateworld.domain
+
+import enumeratum.{Enum, EnumEntry}
+
+import java.time.temporal.ChronoUnit
+
+sealed abstract class DateTimeUnit(val suffix: String, val chronoUnit: ChronoUnit) extends EnumEntry
+
+object DateTimeUnit extends Enum[DateTimeUnit] {
+  override val values: IndexedSeq[DateTimeUnit] = findValues
+
+  def fromString(str: String): Option[DateTimeUnit] = values.find(_.entryName == str)
+
+  case object YEAR extends DateTimeUnit("y", ChronoUnit.YEARS)
+
+  case object MONTH extends DateTimeUnit("mo", ChronoUnit.MONTHS)
+
+  case object WEEK extends DateTimeUnit("w", ChronoUnit.WEEKS)
+
+  case object DAY_OF_MONTH extends DateTimeUnit("d", ChronoUnit.DAYS)
+
+  case object HOUR extends DateTimeUnit("h", ChronoUnit.HOURS)
+
+  case object MINUTE extends DateTimeUnit("m", ChronoUnit.MINUTES)
+}
