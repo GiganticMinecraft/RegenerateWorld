0f3bff573e3170eca78c6cb26dccd2781addb456
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-08-23 14:32:24 +0900

    feat: impl some method for Usecase

--- src/main/scala/click/seichi/regenerateworld/usecase/GenerationScheduleUseCase.scala
+++ src/main/scala/click/seichi/regenerateworld/usecase/GenerationScheduleUseCase.scala
@@ -1,9 +1,48 @@
 package click.seichi.regenerateworld.usecase
 
-import click.seichi.regenerateworld.domain.{GenerationSchedule, UseGenerateScheduleRepository}
+import click.seichi.regenerateworld.domain.{GenerationSchedule, SeedPattern}
+import click.seichi.regenerateworld.usecase.usetraits.{UseClock, UseGenerationScheduleRepository}
 
-trait GenerationScheduleUseCase extends UseGenerateScheduleRepository {
-  def list(): Set[GenerationSchedule] = generateScheduleRepository.list()
+import java.util.UUID
+
+trait GenerationScheduleUseCase extends UseGenerationScheduleRepository with UseClock {
+  def list(): Set[GenerationSchedule] = generationScheduleRepository.list()
 
   def filteredList(predicate: GenerationSchedule => Boolean): Set[GenerationSchedule] = list().filter(predicate)
+
+  def findById(id: UUID): Option[GenerationSchedule] = generationScheduleRepository.find(id)
+
+  def remove(id: UUID): Boolean = generationScheduleRepository.remove(id)
+
+  def addWorld(id: UUID, worldName: String): Unit = {
+    for {
+      schedule <- findById(id)
+      worlds = schedule.worlds + worldName
+      newSchedule = GenerationSchedule(schedule.id, schedule.nextDateTime, schedule.interval, schedule.seedPattern, worlds)
+    } yield generationScheduleRepository.save(newSchedule)
+  }
+
+  def removeWorld(id: UUID, worldName: String): Unit = {
+    for {
+      schedule <- findById(id)
+      worlds = schedule.worlds.filterNot(_ == worldName)
+      newSchedule = GenerationSchedule(schedule.id, schedule.nextDateTime, schedule.interval, schedule.seedPattern, worlds)
+    } yield generationScheduleRepository.save(newSchedule)
+  }
+
+  def finish(id: UUID): Unit = {
+    for {
+      schedule <- findById(id)
+      newSchedule = schedule.finish(clock.now)
+    } yield generationScheduleRepository.save(newSchedule)
+  }
+
+  def changeInterval(): Unit = ???
+
+  def changeSeedPattern(id: UUID, pattern: SeedPattern): Unit = {
+    for {
+      schedule <- findById(id)
+      newSchedule = GenerationSchedule(schedule.id, schedule.nextDateTime, schedule.interval, pattern, schedule.worlds)
+    } yield generationScheduleRepository.save(newSchedule)
+  }
 }
