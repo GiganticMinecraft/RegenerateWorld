5ad50a9a298db4ed3bb3a90e36364b4bdc650e55
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-08-24 23:40:03 +0900

    feat: add Multiverse object

---
+++ src/main/scala/click/seichi/regenerateworld/presenter/shared/Multiverse.scala
@@ -0,0 +1,19 @@
+package click.seichi.regenerateworld.presenter.shared
+
+import click.seichi.regenerateworld.RegenerateWorld.INSTANCE
+import com.onarandombox.MultiverseCore.MultiverseCore
+import com.onarandombox.MultiverseCore.api.MultiverseWorld
+import org.bukkit.World
+
+case object Multiverse {
+  private val instance = INSTANCE.getServer.getPluginManager.getPlugin("Multiverse-Core").asInstanceOf[MultiverseCore]
+
+  def fromBukkitWorld(world: World): Option[MultiverseWorld] = Option(instance.getMVWorldManager.getMVWorld(world))
+
+  def regenWorld(world: MultiverseWorld, isNewSeed: Boolean, isRandomSeed: Boolean, newSeed: Option[String]): Boolean
+    = instance.getMVWorldManager.regenWorld(world.getName, isNewSeed, isRandomSeed, newSeed.orNull)
+
+  implicit class BukkitWorldOps(val world: World) {
+    def asMVWorld(): Option[MultiverseWorld] = fromBukkitWorld(world)
+  }
+}
