866f68e7967c18de03ae245816a5f55691b90dea
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-08-25 20:59:53 +0900

    style: fix indents

--- src/main/scala/click/seichi/regenerateworld/presenter/shared/Multiverse.scala
+++ src/main/scala/click/seichi/regenerateworld/presenter/shared/Multiverse.scala
@@ -6,24 +6,25 @@ import com.onarandombox.MultiverseCore.api.MultiverseWorld
 import org.bukkit.World
 
 case object Multiverse {
-  private val instance = INSTANCE
-    .getServer
-    .getPluginManager
-    .getPlugin("Multiverse-Core")
-    .asInstanceOf[MultiverseCore]
+  private val instance =
+    INSTANCE
+      .getServer
+      .getPluginManager
+      .getPlugin("Multiverse-Core")
+      .asInstanceOf[MultiverseCore]
 
-  def fromBukkitWorld(world: World): Option[MultiverseWorld] = Option(
-    instance.getMVWorldManager.getMVWorld(world)
-  )
+  def fromBukkitWorld(world: World): Option[MultiverseWorld] =
+    Option(instance.getMVWorldManager.getMVWorld(world))
 
   def regenWorld(
     world: MultiverseWorld,
     isNewSeed: Boolean,
     isRandomSeed: Boolean,
     newSeed: Option[String]
-  ): Boolean = instance
-    .getMVWorldManager
-    .regenWorld(world.getName, isNewSeed, isRandomSeed, newSeed.orNull)
+  ): Boolean =
+    instance
+      .getMVWorldManager
+      .regenWorld(world.getName, isNewSeed, isRandomSeed, newSeed.orNull)
 
   implicit class BukkitWorldOps(val world: World) {
     def asMVWorld(): Option[MultiverseWorld] = fromBukkitWorld(world)
