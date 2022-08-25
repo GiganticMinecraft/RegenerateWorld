7448376b55a09cd6011400e4769224840d2915cd
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-08-25 15:30:24 +0900

    fix: add ChatColor

--- src/main/scala/click/seichi/regenerateworld/presenter/shared/contextualexecutor/ContextualExecutor.scala
+++ src/main/scala/click/seichi/regenerateworld/presenter/shared/contextualexecutor/ContextualExecutor.scala
@@ -1,6 +1,7 @@
 package click.seichi.regenerateworld.presenter.shared.contextualexecutor
 
 import click.seichi.regenerateworld.presenter.shared.exception.LangException
+import org.bukkit.ChatColor
 import org.bukkit.command.{Command, CommandSender, TabExecutor}
 
 import scala.jdk.CollectionConverters._
@@ -14,6 +15,8 @@ trait ContextualExecutor {
 object ContextualExecutor {
   implicit class ContextualTabExecutor(val contextualExecutor: ContextualExecutor) {
     def asTabExecutor: TabExecutor = new TabExecutor {
+      private def addChatColor(str: String): String = s"${ChatColor.RED}s$str"
+
       override def onCommand(commandSender: CommandSender, command: Command, alias: String, args: Array[String]): Boolean = {
         val context = CommandContext(commandSender, command, args.toList)
         val errMessage = s"The exception has thrown while executing ${command.getName} command in RegenerateWorld"
@@ -23,9 +26,10 @@ object ContextualExecutor {
             println(errMessage)
             err.throwable.printStackTrace()
             Seq(errMessage, "Please contact to the server administrators", s"details: ${err.description}")
+              .map(addChatColor)
               .foreach(commandSender.sendMessage)
           case Left(err) =>
-            Seq(errMessage, err.description).foreach(commandSender.sendMessage)
+            Seq(errMessage, err.description).map(addChatColor).foreach(commandSender.sendMessage)
           case Right(_) =>
         }
 
