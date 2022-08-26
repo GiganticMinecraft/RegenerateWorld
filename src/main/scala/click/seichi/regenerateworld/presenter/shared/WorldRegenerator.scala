ea81016348fc74e6c994fc186053eadca2dbfe09
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-08-26 15:01:15 +0900

    fix: call WorldRegenEvent when regen

--- src/main/scala/click/seichi/regenerateworld/presenter/shared/WorldRegenerator.scala
+++ src/main/scala/click/seichi/regenerateworld/presenter/shared/WorldRegenerator.scala
@@ -2,9 +2,10 @@ package click.seichi.regenerateworld.presenter.shared
 
 import click.seichi.regenerateworld.domain.model.SeedPattern
 import Multiverse._
+import click.seichi.regenerateworld.presenter.event.{PreRegenWorldEvent, RegenWorldEvent}
 import click.seichi.regenerateworld.presenter.shared.exception.WorldRegenerationException
 import com.onarandombox.MultiverseCore.api.MultiverseWorld
-import org.bukkit.World
+import org.bukkit.{Bukkit, World}
 
 case object WorldRegenerator {
   def regen(
@@ -15,8 +16,13 @@ case object WorldRegenerator {
     if (seedPattern == SeedPattern.NewSeed && newSeed.isEmpty)
       return Left(WorldRegenerationException.SeedIsRequired)
 
+    val preEvent = PreRegenWorldEvent(world.getName)
+    Bukkit.getPluginManager.callEvent(preEvent)
+    if (preEvent.isCancelled) return Left(WorldRegenerationException.RegenWorldEventIsCancelled)
+
     val isSuccessful =
       Multiverse.regenWorld(world, seedPattern.isNewSeed, seedPattern.isRandomSeed, newSeed)
+    Bukkit.getPluginManager.callEvent(RegenWorldEvent(world.getName))
     if (isSuccessful) Right(()) else Left(WorldRegenerationException.MultiverseException)
   }
 
