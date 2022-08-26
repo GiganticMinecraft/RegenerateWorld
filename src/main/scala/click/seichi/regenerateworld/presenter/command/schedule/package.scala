5bd7cdf915141ad1f34d9e140aebeb521bba271a
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-08-26 17:08:51 +0900

    style: run scalafmt

--- src/main/scala/click/seichi/regenerateworld/presenter/command/schedule/package.scala
+++ src/main/scala/click/seichi/regenerateworld/presenter/command/schedule/package.scala
@@ -1,15 +1,15 @@
 package click.seichi.regenerateworld.presenter.command
 
-import click.seichi.regenerateworld.presenter.shared.contextualexecutor.executor.{BranchedExecutor, EchoExecutor}
+import click.seichi.regenerateworld.presenter.shared.contextualexecutor.executor.{
+  BranchedExecutor,
+  EchoExecutor
+}
 
 package object schedule {
   val help: EchoExecutor = EchoExecutor(
     List("/rw schedule", "    再生成スケジュールを追加・変更・削除します。詳細は「/rw schedule help」を実行してください。")
   )
 
-  val executor: BranchedExecutor = BranchedExecutor(
-    Map("remove" -> Remove, "help" -> Help),
-    Some(Help),
-    Some(Help)
-  )
+  val executor: BranchedExecutor =
+    BranchedExecutor(Map("remove" -> Remove, "help" -> Help), Some(Help), Some(Help))
 }
