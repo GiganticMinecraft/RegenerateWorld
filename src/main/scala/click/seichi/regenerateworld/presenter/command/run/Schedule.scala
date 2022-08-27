4cee934f4fd9887e2d72b95f0d250d620d56d737
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-08-27 12:07:23 +0900

    fix: /regen run schedule update nextDateTime of schedule

--- src/main/scala/click/seichi/regenerateworld/presenter/command/run/Schedule.scala
+++ src/main/scala/click/seichi/regenerateworld/presenter/command/run/Schedule.scala
@@ -39,6 +39,7 @@ case object Schedule extends ContextualExecutor {
         regenStartMessages(worldName).foreach(context.sender.sendMessage)
 
         WorldRegenerator.regenFromWorld(w, schedule.seedPattern, newSeed).onSuccess { _ =>
+          GenerationScheduleUseCase.finish(schedule.id)
           context.sender.sendMessage(regenSuccessfulMessage(worldName))
         }
       case _ => Left(WorldRegenerationException.WorldIsNotFound(worldName))
