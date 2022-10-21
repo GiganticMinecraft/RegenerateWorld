524a7757dd31cf97af991296646aa88cdac841f8
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-10-21 22:41:36 +0900

    feat: kick player when spawn world is not found or target world

--- src/main/scala/click/seichi/regenerateworld/presenter/listener/RegenWorldListener.scala
+++ src/main/scala/click/seichi/regenerateworld/presenter/listener/RegenWorldListener.scala
@@ -2,8 +2,11 @@ package click.seichi.regenerateworld.presenter.listener
 
 import click.seichi.regenerateworld.presenter.Setting
 import click.seichi.regenerateworld.presenter.event.{PreRegenWorldEvent, RegenWorldEvent}
+import click.seichi.regenerateworld.presenter.shared.external.Multiverse
 import org.bukkit.Bukkit
 import org.bukkit.event.{EventHandler, Listener}
+
+import java.util.Collections.emptyList
 
 case object RegenWorldListener extends Listener {
   private def replaceWithWorldName(s: String, worldName: String) =
@@ -11,27 +14,36 @@ case object RegenWorldListener extends Listener {
 
   @EventHandler
   def onPreRegenWorld(event: PreRegenWorldEvent): Unit = {
-    for {
-      setting <- Setting.settingRepository.get()
-      _ = setting
-        .beforeCommand
-        .map(replaceWithWorldName(_, event.worldName))
-        .foreach(Bukkit.dispatchCommand(Bukkit.getConsoleSender, _))
-      world <- Option(Bukkit.getWorld(event.worldName))
-      worldName = setting.teleportWorldName
-      teleportWorld <- Option(Bukkit.getWorld(worldName))
-      _ = world.getPlayers.forEach { player => player.teleport(teleportWorld.getSpawnLocation) }
-    } yield ()
+    val setting = Setting.get()
+
+    setting
+      .map(_.beforeCommand)
+      .getOrElse(Set())
+      .map(replaceWithWorldName(_, event.worldName))
+      .foreach(Bukkit.dispatchCommand(Bukkit.getConsoleSender, _))
+
+    val players =
+      Option(Bukkit.getWorld(event.worldName)).map(_.getPlayers).getOrElse(emptyList)
+    setting
+      .map(_.teleportWorldName)
+      .flatMap(name => Option(Bukkit.getWorld(name)))
+      .filter(_.getName.toLowerCase != event.worldName.toLowerCase) match {
+      case Some(world) =>
+        players.forEach(Multiverse.teleportPlayer(_, world.getSpawnLocation))
+      case _ =>
+        players.forEach(
+          _.kickPlayer(s"Regenerating the world (${event.worldName}) you were in")
+        )
+    }
   }
 
   @EventHandler
   def onRegenWorld(event: RegenWorldEvent): Unit = {
-    for {
-      setting <- Setting.settingRepository.get()
-      _ = setting
-        .afterCommand
-        .map(replaceWithWorldName(_, event.worldName))
-        .foreach(Bukkit.dispatchCommand(Bukkit.getConsoleSender, _))
-    } yield ()
+    Setting
+      .get()
+      .map(_.afterCommand)
+      .getOrElse(Set())
+      .map(replaceWithWorldName(_, event.worldName))
+      .foreach(Bukkit.dispatchCommand(Bukkit.getConsoleSender, _))
   }
 }
