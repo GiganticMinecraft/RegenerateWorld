d1a18639e8025d399e97f33feea9ead6d40bc301
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-10-22 16:22:07 +0900

    feat: run regeneration onEnable

--- src/main/scala/click/seichi/regenerateworld/presenter/RegenerateWorld.scala
+++ src/main/scala/click/seichi/regenerateworld/presenter/RegenerateWorld.scala
@@ -2,17 +2,46 @@ package click.seichi.regenerateworld.presenter
 
 import click.seichi.regenerateworld.presenter.command.Command
 import click.seichi.regenerateworld.presenter.listener.RegenWorldListener
+import click.seichi.regenerateworld.presenter.runnable.RegenerationTask
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
+
+import java.time.ZonedDateTime
 
 class RegenerateWorld extends JavaPlugin {
   override def onEnable(): Unit = {
-    RegenerateWorld.INSTANCE = this
+    RegenerateWorld.instance = this
 
     saveDefaultConfig()
 
     getCommand("regenworld").setExecutor(Command.executor)
     getServer.getPluginManager.registerEvents(RegenWorldListener, this)
-    // TODO: register runnable per each schedule
+
+    Bukkit.getLogger.info("RegenerateWorld is enabled!")
+
+    runRegeneration()
+  }
+
+  override def onDisable(): Unit = {
+    // TODO: cancel all tasks
+
+    Bukkit.getLogger.info("RegenerateWorld is disabled!")
+  }
+
+  private def runRegeneration(): Unit = {
+    implicit val instance: RegenerateWorld = this
+
+    GenerationScheduleUseCase
+      .filteredList(schedule => {
+        val nextDateTime = schedule.nextDateTime
+        val now = ZonedDateTime.now()
+
+        nextDateTime.isBefore(now) || nextDateTime.isEqual(now)
+      })
+      .foreach(RegenerationTask.runInstantly)
+
+    // TODO: Add・Edit・Deleteされた際に変更を反映させる必要があるので、TaskIdを持っておきたい
+    GenerationScheduleUseCase.list().foreach(RegenerationTask.runAtNextDate)
   }
 }
 
