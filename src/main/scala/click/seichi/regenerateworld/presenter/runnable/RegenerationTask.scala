7cb558574944c2e46fb458dc960f74a81363cbdc
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-11-07 23:56:59 +0900

    feat: add task(WIP)

--- src/main/scala/click/seichi/regenerateworld/presenter/runnable/RegenerationTask.scala
+++ src/main/scala/click/seichi/regenerateworld/presenter/runnable/RegenerationTask.scala
@@ -1,15 +1,19 @@
 package click.seichi.regenerateworld.presenter.runnable
 
+import cats.syntax.all._
+import cats.implicits._
+import cats.effect.{Async, IO, Sync}
 import click.seichi.regenerateworld.domain.model.{GenerationSchedule, SeedPattern}
 import click.seichi.regenerateworld.presenter.GenerationScheduleUseCase
 import click.seichi.regenerateworld.presenter.mixin.MixInClock
 import click.seichi.regenerateworld.presenter.shared.WorldRegenerator
 import click.seichi.regenerateworld.presenter.shared.exception.WorldRegenerationException
-import org.bukkit.{Bukkit, ChatColor}
+import org.bukkit.{Bukkit, ChatColor, World}
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.scheduler.{BukkitRunnable, BukkitTask}
 
 import java.time.temporal.ChronoUnit
+import scala.concurrent.duration.DurationInt
 
 object RegenerationTask extends MixInClock {
   def runAtNextDate(schedule: GenerationSchedule)(implicit instance: JavaPlugin): BukkitTask = {
@@ -21,47 +25,99 @@ object RegenerationTask extends MixInClock {
     }.runTaskLaterAsynchronously(instance, difference * 20L)
   }
 
-  def runInstantly(schedule: GenerationSchedule)(implicit instance: JavaPlugin): BukkitTask = {
-    new RegenerationTask(schedule).runTaskTimerAsynchronously(instance, 0L, 20L)
-  }
+  def runInstantly(schedule: GenerationSchedule)(implicit instance: JavaPlugin) =
+    new RegenerationTask(schedule).ooo[IO]()
 }
 
 private class RegenerationTask(private val schedule: GenerationSchedule)(
   implicit instance: JavaPlugin
-) extends BukkitRunnable {
-  private var count = 10
+) {
 
-  override def run(): Unit = {
-    if (count == 0) {
-      for {
-        worldName <- schedule.worlds
-        world = Option(Bukkit.getWorld(worldName))
-          .toRight(WorldRegenerationException.WorldIsNotFound(worldName))
-        seedPattern =
-          if (schedule.seedPattern.seedValueIsRequiredExplicitly) SeedPattern.RandomNewSeed
-          else schedule.seedPattern
-        result = world.flatMap(w => WorldRegenerator.regenBukkitWorld(w, seedPattern, None))
-        logger = instance.getLogger
-      } yield result match {
-        case Right(_) =>
-          logger.info(
-            s"The world ($worldName) regeneration schedule (ID: ${schedule.id}) has completed"
-          )
-        case Left(e) =>
-          logger.severe(
-            s"Error has occurred while regenerating a world ($worldName) with the schedule (ID: ${schedule.id}): ${e.description}"
-          )
+  private def aaaa[F[_]: Async](worldName: String) =
+    for {
+      world <- Async[F].delay(Option(Bukkit.getWorld(worldName)))
+      world <- world match {
+        case Some(world) => Async[F].pure(world)
+        case None => Async[F].raiseError(WorldRegenerationException.WorldIsNotFound(worldName))
+      }
+      seedPattern =
+        if (schedule.seedPattern.seedValueIsRequiredExplicitly) SeedPattern.RandomNewSeed
+        else schedule.seedPattern
+      _ <- Sync[F].blocking(WorldRegenerator.regenBukkitWorld(world, seedPattern, None))
+    } yield worldName
+
+  def ooo[F[_]: Async]() =
+    for {
+      count <- 10 to 0 by -1
+    } yield for {
+      _ <-
+        Async[F]
+          .delay {
+            Bukkit.broadcastMessage(
+              s"${ChatColor.RED}The regeneration of ${schedule.worlds.mkString(", ")} will be held in $count minute(s)!"
+            )
+          }
+          .whenA(Set(1, 2, 3, 5, 10).contains(count))
+      _ <- Async[F].delay(schedule.worlds.toList.traverse(aaaa)).whenA(count == 0)
+      _ <- Async[F].sleep(1.seconds)
+    } yield ()
+
+  def run(): Unit =
+    for (count <- 10 to 0 by -1) {
+      if (count == 0) {
+        val regenResults = for {
+          worldName <- schedule.worlds
+        } yield for {
+          world <- Option(Bukkit.getWorld(worldName))
+            .toRight(WorldRegenerationException.WorldIsNotFound(worldName))
+          seedPattern =
+            if (schedule.seedPattern.seedValueIsRequiredExplicitly) SeedPattern.RandomNewSeed
+            else schedule.seedPattern
+          _ = new BukkitRunnable() {
+            override def run(): Unit =
+              WorldRegenerator.regenBukkitWorld(world, seedPattern, None)
+          }.runTask(instance)
+        } yield worldName
+
+        regenResults.foreach(res => {
+          val logger = instance.getLogger
+
+          res match {
+            case Right(worldName) =>
+              logger.info(
+                s"The world (name: $worldName) regeneration schedule (ID: ${schedule.id}) has completed"
+              )
+            case Left(e) =>
+              logger.severe(
+                s"Error has occurred while regenerating a world with the schedule (ID: ${schedule.id}): ${e.description}"
+              )
+          }
+        })
+
+        // Scheduleの次回日時の設定は1回でいいので、for-yieldの外に置く
+        new BukkitRunnable {
+          override def run(): Unit = GenerationScheduleUseCase.finish(schedule.id)
+        }.runTask(instance)
+      } else if (Set(1, 3, 5, 10).contains(count)) {
+        Bukkit.broadcastMessage(
+          s"${ChatColor.RED}The regeneration of ${schedule.worlds.mkString(", ")} will be held in $count minute(s)!"
+        )
       }
 
-      // RunnableのcancelとScheduleの次回日時の設定は1回でいいので、for-yieldの外に置く
-      GenerationScheduleUseCase.finish(schedule.id)
-      this.cancel()
-    } else if (Set(1, 3, 5, 10).contains(count)) {
-      Bukkit.broadcastMessage(
-        s"${ChatColor.RED}The regeneration of ${schedule.worlds.mkString(", ")} will be held in $count minute(s)!"
-      )
+      // TODO: wait 60 secs
+      Thread.sleep(1 * 1000)
     }
 
-    count -= 1
+  private def regenSync(world: World, seedPattern: SeedPattern) = {
+    var res: Either[WorldRegenerationException, Unit] = Left(
+      WorldRegenerationException.ScheduleIsNotFound
+    )
+    new BukkitRunnable() {
+      override def run(): Unit = {
+        res = WorldRegenerator.regenBukkitWorld(world, seedPattern, None)
+      }
+    }.runTask(instance)
+
+    res
   }
 }
