7b096ac2a18f55657fad71372a76179f76d907c8
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-11-15 17:05:56 +0900

    feat: run RegenerateTask with cat(WIP)

--- src/main/scala/click/seichi/regenerateworld/presenter/RegenerateWorld.scala
+++ src/main/scala/click/seichi/regenerateworld/presenter/RegenerateWorld.scala
@@ -1,6 +1,7 @@
 package click.seichi.regenerateworld.presenter
 
-import click.seichi.regenerateworld.presenter.RegenerateWorld.regenerationTasks
+import scala.concurrent.ExecutionContext.Implicits._
+import click.seichi.regenerateworld.presenter.RegenerateWorld.{instance, regenerationTasks}
 import click.seichi.regenerateworld.presenter.command.Command
 import click.seichi.regenerateworld.presenter.listener.RegenWorldListener
 import click.seichi.regenerateworld.presenter.runnable.RegenerationTask
@@ -11,6 +12,7 @@ import org.bukkit.scheduler.BukkitTask
 import java.time.ZonedDateTime
 import java.util.UUID
 import scala.collection.mutable
+import scala.concurrent.Future
 
 class RegenerateWorld extends JavaPlugin {
   override def onEnable(): Unit = {
@@ -24,6 +26,7 @@ class RegenerateWorld extends JavaPlugin {
     Bukkit.getLogger.info("RegenerateWorld is enabled!")
 
     runRegeneration()
+    instance.getLogger.severe("on enable awaited?")
   }
 
   override def onDisable(): Unit = {
@@ -32,23 +35,43 @@ class RegenerateWorld extends JavaPlugin {
     Bukkit.getLogger.info("RegenerateWorld is disabled!")
   }
 
-  private def runRegeneration(): Unit = {
+  private def runRegeneration(): Unit = Future {
     import RegenerateWorld._
+    import cats.effect.unsafe.implicits._
 
-    GenerationScheduleUseCase
-      .filteredList(schedule => {
+    for {
+      schedule <- GenerationScheduleUseCase.filteredList(schedule => {
         val nextDateTime = schedule.nextDateTime
         val now = ZonedDateTime.now()
 
         nextDateTime.isBefore(now) || nextDateTime.isEqual(now)
       })
-      .foreach(RegenerationTask.runInstantly)
+    } yield RegenerationTask.runInstantly(schedule).unsafeRunAsync(_ => println())
 
-    GenerationScheduleUseCase
-      .list()
-      .map(schedule => (schedule.id, RegenerationTask.runAtNextDate(schedule)))
-      .foreach { case (scheduleId, task) => regenerationTasks.put(scheduleId, task) }
+//    val ft = GenerationScheduleUseCase
+//      .filteredList(schedule => {
+//        val nextDateTime = schedule.nextDateTime
+//        val now = ZonedDateTime.now()
+//
+//        nextDateTime.isBefore(now) || nextDateTime.isEqual(now)
+//      })
+//      .map(schedule => RegenerationTask.runInstantly(schedule))
+
+    // TODO: await fin
+    instance.getLogger.severe("awaited?")
   }
+//    GenerationScheduleUseCase
+//      .list()
+//      .tapEach(schedule => {
+//        val nextDateTime = schedule.nextDateTime
+//        val now = ZonedDateTime.now()
+//
+//        if (nextDateTime.isBefore(now) || nextDateTime.isEqual(now))
+//          RegenerationTask.runInstantly(schedule)
+//      })
+//      .tapEach(_ => instance.getLogger.info("awaited?"))
+//      .map(schedule => (schedule.id, RegenerationTask.runAtNextDate(schedule)))
+//      .foreach { case (scheduleId, task) => regenerationTasks.put(scheduleId, task) }
 }
 
 object RegenerateWorld {
